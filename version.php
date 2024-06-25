f3f9ad82d415ddd5691472cc0cf4d3580934f35b
David Bogner <davidbogner@gmail.com>
2024-06-25 08:28:53 +0200

    New version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_export_api';
-$plugin->release = '0.1.4';
-$plugin->version =  2024062500;
+$plugin->release = '0.1.5';
+$plugin->version =  2024062501;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
